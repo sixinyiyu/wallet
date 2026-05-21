a7f0d83afa0782ec3c651aa54ea83d8fc56d0105
gemcoder21 <104884878+gemcoder21@users.noreply.github.com>
2026-05-21 01:54:37 +0000

    Bump to 2.69 (iOS 126, Android 768)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.gemwallet.android"
         minSdk = 28
         targetSdk = 37
-        versionCode = 767
-        versionName = "2.68"
+        versionCode = 768
+        versionName = "2.69"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
